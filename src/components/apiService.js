cea605074042190196ac98f7a7b50113fbc7d3a6
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-11-02 21:33:04 +0200

    1

---
+++ src/components/apiService.js
@@ -0,0 +1,10 @@
+// import { forEach } from "core-js/fn/array";
+
+export default function (value, page) {
+    const TOKEN = '18953404-219a87b5236596fa40acd8a55'
+    // const page = 1;
+ 
+       return   fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${page}&per_page=12&key=${TOKEN}`)
+            .then((data) => data.json())
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './styles.css';
+
+import template from "./template/templateList.hbs";
+import servis from './components/apiService'
+
+const button=document.querySelector(".button")
+const input = document.querySelector('[type ="text"]')
+const ul =document.querySelector(".gallery")
+let array;
+let page=1;
+
+const inputHeder = function () {
+    servis(input.value, page)
+        .then((data ) => {
+            data.hits.forEach((el) => {
+            button.classList.remove('is-hidden') 
+             ul.insertAdjacentHTML("beforeend", template(el))  
+            });
+      
+        })
+    
+       
+}
+let opo = function () {
+    servis(input.value, page += 1)
+    .then((data ) => {
+            data.hits.forEach((el) => {
+            button.classList.remove('is-hidden') 
+             ul.insertAdjacentHTML("beforeend", template(el))  
+            });
+       
+        })
+}
+   button.addEventListener("click", opo)
+   input.addEventListener('blur', inputHeder)
+
+
+
+
+
+
