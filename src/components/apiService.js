950fb7ab65cb338072ec31104f1d30d3e1ebc56f
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-11-03 21:26:21 +0200

    1

--- src/components/apiService.js
+++ src/components/apiService.js
@@ -2,9 +2,58 @@
 
 export default function (value, page) {
     const TOKEN = '18953404-219a87b5236596fa40acd8a55'
-    // const page = 1;
+   
  
        return   fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${page}&per_page=12&key=${TOKEN}`)
             .then((data) => data.json())
   
 }
+
+
+// import './styles.css';
+
+// import api from "./components/apiService.js"
+// import template from "./template/templateList.hbs"
+
+
+// let btn = document.querySelector(".button")
+// let input = document.querySelector('[type="text"]');
+// let ul = document.querySelector(".gallery")
+// let page = 1;
+
+
+// const addCard = function () {
+//     api(input.value,page)
+//     .then((data) => {
+//             data.hits.forEach(el => {
+//             ul.insertAdjacentHTML("beforeend", template(el))
+//             btn.classList.remove("is-hidden")
+//             });
+//             });
+
+// }
+// let a = 100;
+// const loadMore = function () {
+//             window.scrollTo({
+//             top:document.documentElement.offsetHeight,
+//             behavior: 'smooth'
+//             }) 
+//          api(input.value, page += 1)
+//     .then((data) => {
+//         data.hits.forEach(el => { 
+//             document.documentElement > a + 100 
+//             ul.insertAdjacentHTML("beforeend", template(el))
+//         })
+
+//     });
+
+// }
+// btn.addEventListener("click",loadMore)
+// input.addEventListener("blur", addCard )
+// window.addEventListener('scroll', () => {
+
+//     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+//     if(clientHeight + scrollTop >= scrollHeight - 1) {
+//         loadMore();
+//     }
+// });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './styles.css';
 
 import template from "./template/templateList.hbs";
 import servis from './components/apiService'
+import { alert, info } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css'
 
 const button=document.querySelector(".button")
 const input = document.querySelector('[type ="text"]')
@@ -9,30 +12,57 @@ const ul =document.querySelector(".gallery")
 let array;
 let page=1;
 
+var scrollTop =document.documentElement.scrollTop
+console.log(scrollTop);
+
 const inputHeder = function () {
+       console.log(input.value);
     servis(input.value, page)
-        .then((data ) => {
-            data.hits.forEach((el) => {
-            button.classList.remove('is-hidden') 
-             ul.insertAdjacentHTML("beforeend", template(el))  
-            });
-      
-        })
-    
-       
+        .then((data) => {
+           
+            if (data.hits.length>=1) {
+                data.hits.forEach((el) => {
+                    button.classList.remove('is-hidden')
+                    ul.insertAdjacentHTML("beforeend", template(el))
+                });
+            } else {
+                info('     Enter another name!')
+                ul.innerHTML = ""
+                button.classList.add('is-hidden')
+            }
+          console.log(data.hits);
+        })    
+                ul.innerHTML = ""
+                button.classList.add('is-hidden')
 }
-let opo = function () {
-    servis(input.value, page += 1)
-    .then((data ) => {
-            data.hits.forEach((el) => {
-            button.classList.remove('is-hidden') 
-             ul.insertAdjacentHTML("beforeend", template(el))  
-            });
-       
-        })
+const loadMore = function () {
+      servis(input.value, page += 1)
+        .then((data) => {
+            data.hits.forEach(el => {
+                document.documentElement >100
+                ul.insertAdjacentHTML("beforeend", template(el))
+            })
+
+        });
 }
-   button.addEventListener("click", opo)
-   input.addEventListener('blur', inputHeder)
+
+const down = function () {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+        if (clientHeight + scrollTop >= scrollHeight - 1) {    
+            loadMore();
+            
+            const fn = function () {
+                window.scrollBy(0, 600);
+                behavior: "smooth"
+            };
+      setTimeout(fn, 75)       
+           
+    }
+}
+
+   button.addEventListener("click", loadMore)
+   input.addEventListener('change', inputHeder)
+  window.addEventListener('scroll',down);
 
 
 
