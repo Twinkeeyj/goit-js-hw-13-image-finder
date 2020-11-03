9762057e651986e11c7c488f86b7c560fb13b469
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-11-03 21:59:11 +0200

    finish

--- src/components/apiService.js
+++ src/components/apiService.js
@@ -1,5 +1,3 @@
-// import { forEach } from "core-js/fn/array";
-
 export default function (value, page) {
     const TOKEN = '18953404-219a87b5236596fa40acd8a55'
    
@@ -8,52 +6,3 @@ export default function (value, page) {
             .then((data) => data.json())
   
 }
-
-
-// import './styles.css';
-
-// import api from "./components/apiService.js"
-// import template from "./template/templateList.hbs"
-
-
-// let btn = document.querySelector(".button")
-// let input = document.querySelector('[type="text"]');
-// let ul = document.querySelector(".gallery")
-// let page = 1;
-
-
-// const addCard = function () {
-//     api(input.value,page)
-//     .then((data) => {
-//             data.hits.forEach(el => {
-//             ul.insertAdjacentHTML("beforeend", template(el))
-//             btn.classList.remove("is-hidden")
-//             });
-//             });
-
-// }
-// let a = 100;
-// const loadMore = function () {
-//             window.scrollTo({
-//             top:document.documentElement.offsetHeight,
-//             behavior: 'smooth'
-//             }) 
-//          api(input.value, page += 1)
-//     .then((data) => {
-//         data.hits.forEach(el => { 
-//             document.documentElement > a + 100 
-//             ul.insertAdjacentHTML("beforeend", template(el))
-//         })
-
-//     });
-
-// }
-// btn.addEventListener("click",loadMore)
-// input.addEventListener("blur", addCard )
-// window.addEventListener('scroll', () => {
-
-//     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-//     if(clientHeight + scrollTop >= scrollHeight - 1) {
-//         loadMore();
-//     }
-// });
--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import './styles.css';
 
 import template from "./template/templateList.hbs";
 import servis from './components/apiService'
-import { alert, info } from '@pnotify/core';
+import { alert} from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css'
 
 const button=document.querySelector(".button")
-const input = document.querySelector('[type ="text"]')
+let input = document.querySelector('input')
 const ul =document.querySelector(".gallery")
-let array;
+
 let page=1;
 
 var scrollTop =document.documentElement.scrollTop
-console.log(scrollTop);
 
 const inputHeder = function () {
        console.log(input.value);
@@ -26,7 +25,7 @@ const inputHeder = function () {
                     ul.insertAdjacentHTML("beforeend", template(el))
                 });
             } else {
-                info('     Enter another name!')
+                alert('     Enter another name!')
                 ul.innerHTML = ""
                 button.classList.add('is-hidden')
             }
@@ -52,17 +51,16 @@ const down = function () {
             loadMore();
             
             const fn = function () {
-                window.scrollBy(0, 600);
+                window.scrollBy(0, 672);
                 behavior: "smooth"
             };
       setTimeout(fn, 75)       
            
     }
 }
-
    button.addEventListener("click", loadMore)
    input.addEventListener('change', inputHeder)
-  window.addEventListener('scroll',down);
+   window.addEventListener('scroll',down);
 
 
 
